e7627cb4aa7ad2179c653db8a172dcd31bdcd248
Nhật Vũ <vunhat4869@gmail.com>
2026-03-18 14:19:07 +0700

    update web with env.config to track env variable with t3-oss/env-nextjs

---
+++ apps/web/config/env.config.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import z from "zod"
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+  },
+
+  client: {},
+
+  experimental__runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
+})
