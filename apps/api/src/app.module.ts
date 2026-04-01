54dabfbc82869422dacf82ee338df611233bc44c
Nhật Vũ <vunhat4869@gmail.com>
2026-04-01 13:22:46 +0700

    adding nestjs interceptor for transform res and tiemout error

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,18 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AllExceptionFilter } from '@/common/filter/all-exception/all-exception.filter';
 import { LoggerMiddleware } from '@/common/middleware/logger/logger.middleware';
-import { UserModule } from './user/user.module';
 import { UsersModule } from './users/users.module';
+import { TransformInterceptor } from '@/common/interceptor/transform/transform.interceptor';
+import { TimeoutInterceptor } from '@/common/interceptor/timeout/timeout.interceptor';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UserModule, UsersModule],
+  imports: [AuthModule, DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [
     AppService,
-    { provide: APP_FILTER, useClass: AllExceptionFilter },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
+    },
   ],
 })
 export class AppModule implements NestModule {
