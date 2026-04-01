089fcce74a852f8685a3ebfe2c422d4e28e6e87d
Nhật Vũ <vunhat4869@gmail.com>
2026-04-01 13:26:55 +0700

    clean up global dependency injecttion

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AllExceptionFilter } from '@/common/filter/all-exception/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,8 +12,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  const httpAdapter = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   await app.listen(process.env.PORT ?? 8008);
 }
 bootstrap();
